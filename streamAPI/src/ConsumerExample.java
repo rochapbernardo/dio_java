3ea682ec66362e1c4fe4e7e91d04758a5391f3cf
rochapbernardo <bernardopolese2809@gmail.com>
2025-02-24 14:47:54 -0300

    replace: replace ../streamAPI/PredicateExample.java

--- streamAPI/src/ConsumerExample.java
+++ streamAPI/src/ConsumerExample.java
@@ -11,12 +11,15 @@ public class ConsumerExample {
                 System.out.println(numero);
             }
         };
-        numeros.stream().forEach(imprimirNumeroPar);*/
+        numeros.stream().forEach(imprimirNumeroPar);
 
         numeros.forEach(n ->{
             if(n % 2 == 0){
                 System.out.println(n);
             }
-        });
+        });*/
+        numeros.stream()
+                .filter(n -> n%2==0)
+                .forEach(System.out::println);
     }
 }
